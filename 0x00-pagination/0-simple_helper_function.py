ebde5911c4409d55031393dc7f69b653e865758f
Orjihenry <orjihen@yahoo.com>
2024-01-01 10:46:32 -0330

    Simple helper function 🌏 📦 🌏

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' Pagination '''
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    ''' Returns data '''
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
