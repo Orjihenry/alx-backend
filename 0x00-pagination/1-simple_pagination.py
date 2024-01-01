04436373eadcafe64cda5c58946fbbf345e28181
Orjihenry <orjihen@yahoo.com>
2024-01-01 11:50:01 -0330

    Simple pagination ✌️ 🌏 😜

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,8 +37,6 @@ class Server:
 
         start, end = index_range(page, page_size)
         data = self.dataset()
-
         if start > len(data):
             return []
-
         return data[start:end]
