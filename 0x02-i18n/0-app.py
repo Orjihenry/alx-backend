a82ea3ccfc6811853959ff2b2edf36dd72054f68
Orjihenry <orjihen@yahoo.com>
2024-01-10 23:41:36 -0330

    Basic Flask app 😎 🧐 👍

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashe=False)
-def hello_hbnb():
+def hello_hbnb() -> str:
     ''' Returns the index template '''
-    return render_template('index.html')
+    return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     ''' Main function '''
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
